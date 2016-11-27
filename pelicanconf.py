21a4786f94998f6103083777871adb41839842d7
Shamikh Hossain <shamikh.hossain@duke.edu>
2016-11-27 13:25:13 -0500

    update

--- pelicanconf.py
+++ pelicanconf.py
@@ -20,13 +20,11 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('Twitter', 'http://getpelican.com/'),
+         ('GitHub', 'http://python.org/'),
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
+SOCIAL = (('Emails', '#'),
           ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
